c1aec8750b76dc23d15f2981c0e55a888f2d4855
Christopher Torgalson <manager@bedlamhotel.com>
2018-02-22 10:12:04 +0100

    Adds basic working, tests-passing role

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,22 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+import pytest
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("path", [
+    "/home/molecule/.config/konsolerc",
+    "/home/molecule/.local/share/konsole",
+    "/home/molecule/.vimrc.after",
+    "/home/molecule/.vimrc.before",
+])
+def test_packages(host, path):
+    f = host.file(path)
+
+    assert f.exists
+    assert f.user == 'molecule'
+    assert f.group == 'molecule'
