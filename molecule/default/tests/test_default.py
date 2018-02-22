268d913c767925f426c9d238e9504238d194a8d9
Christopher Torgalson <manager@bedlamhotel.com>
2018-02-22 11:46:00 +0100

    Regularizes quote type in tests file

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,11 +8,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize("path", [
-    "/home/molecule/.config/konsolerc",
-    "/home/molecule/.local/share/konsole",
-    "/home/molecule/.vimrc.after",
-    "/home/molecule/.vimrc.before",
+@pytest.mark.parametrize('path', [
+    '/home/molecule/.config/konsolerc',
+    '/home/molecule/.local/share/konsole',
+    '/home/molecule/.vimrc.after',
+    '/home/molecule/.vimrc.before',
 ])
 def test_packages(host, path):
     f = host.file(path)
